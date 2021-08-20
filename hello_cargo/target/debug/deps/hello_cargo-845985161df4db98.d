af1db0afb3cc8ecfcbb041d49d893e3631cb50ef
Lapi <tyoutugai.7@gmail.com>
2021-08-20 16:07:02 +0900

    section 2

--- hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d
+++
@@ -1,5 +0,0 @@
-/home/lapi/programs/rust-study/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98: src/main.rs
-
-/home/lapi/programs/rust-study/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/lapi/programs/rust-study/hello_cargo/target/debug/hello_cargo: /home/lapi/programs/rust-study/hello_cargo/src/main.rs
