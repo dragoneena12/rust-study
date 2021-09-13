31c71862390d82523ba3f792fc399faa144f1e3a
Lapi <tyoutugai.7@gmail.com>
2021-09-13 17:31:31 +0900

    ignore

--- restaurant/target/rls/debug/deps/restaurant-49ee45a7169087cb.d
+++
@@ -1,7 +0,0 @@
-/home/lapi/programs/rust-study/restaurant/target/rls/debug/deps/restaurant-49ee45a7169087cb.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/home/lapi/programs/rust-study/restaurant/target/rls/debug/deps/restaurant-49ee45a7169087cb.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-src/lib.rs:
-src/front_of_house.rs:
-src/front_of_house/hosting.rs:
--- restaurant/target/rls/debug/deps/restaurant-a93438503595847a.d
+++
@@ -1,7 +0,0 @@
-/home/lapi/programs/rust-study/restaurant/target/rls/debug/deps/restaurant-a93438503595847a.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-/home/lapi/programs/rust-study/restaurant/target/rls/debug/deps/restaurant-a93438503595847a.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
-
-src/lib.rs:
-src/front_of_house.rs:
-src/front_of_house/hosting.rs:
